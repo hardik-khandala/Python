701b83f48b1d44f8519cf68b630e80cf6b254c89
Hardik Khandala <90664350+hardik-khandala@users.noreply.github.com>
2023-07-14 10:45:59 +0530

    Update 3-11.py

--- 3-11.py
+++ 3-11.py
@@ -6,9 +6,23 @@ for i in range(1, n+1):
         print(j, end=" ")
     print()
 
+# output
+# 1
+# 1 2
+# 1 2 3 
+# 1 2 3 4
+# 1 2 3 4 5
+
 print()
 print("Second Output")
 for i in range(1, n+1):
     for j in range(1, n-i+2):
         print("*", end=" ")
     print()
+
+# output
+# * * * * *
+# * * * *
+# * * *
+# * *
+# *
