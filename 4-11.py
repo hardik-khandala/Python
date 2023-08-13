791b5634ab03deac4fe06a7a21326075bd3850b4
YASH CHAUDHARI <119956179+Yashchaudhari29@users.noreply.github.com>
2023-08-13 11:41:16 +0530

    Update 4-11.py

--- 4-11.py
+++ 4-11.py
@@ -1,5 +1,9 @@
-list = [1,2,3,5,2,6,4,2,7,5,2,1,4]
+lst=[]
+n=int(input("Enter the number of values upto you want to add element into element: "))
+for i in range (n):
+    lst.append(int(input("enter: ")))
 
-list1 = set(list)
+print("List With Duplicacy",lst)
+mySet=set(lst)
+print("List Without Duplicacy",lst1:=list(mySet)) #directly declaring using walrus operator
 
-print(list1)
